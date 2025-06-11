edfaaf8c54f0adc850fafb1ae971548eb7b858eb
Lazy Sock <paul.k@unity-mail.de>
2025-06-11 20:24:16 +0200

    add basic layout for AI-section on the landing page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -67,9 +67,101 @@ export default function Home() {
           id="artificial-intelligence"
           className="bg-primary min-h-[900px] text-white"
         >
-          <h2 className="text-center text-[2rem] font-bold lg:text-[3rem]">
+          <h2 className="mb-8 text-center text-[2rem] font-semibold lg:text-[3rem]">
             Artificial Intelligence
           </h2>
+          <p className="mx-auto mb-10 max-w-[1000px] text-center text-[1.25rem]">
+            We created an AI algorithm to classify birds. That's not a
+            particularly new idea. But this task is done quite well. The AI not
+            only classifies birds, but is also able to learn new species by
+            itself. It was trained with images from an image generation model
+            and still recognizes real birds easily.
+          </p>
+          <h3 className="text-center text-[2.5rem]">Statistics</h3>
+          <h3 className="text-center text-[2.5rem]">Want to learn more?</h3>
+          <div className="flex w-full flex-col justify-center gap-[100px] lg:flex-row">
+            <div>
+              <button className="hover:cursor-pointer">
+                <svg
+                  id="visual"
+                  viewBox="0 0 450 450"
+                  width="450"
+                  height="450"
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="450"
+                    height="450"
+                    fill="#204116"
+                  ></rect>
+                  <g transform="translate(204.38153791676166 225.8110429923688)">
+                    <path
+                      d="M118.4 -135.5C155.3 -110.1 188.4 -74.6 200.4 -31.6C212.4 11.3 203.2 61.6 177.9 100.1C152.6 138.6 111.2 165.1 68.4 175.1C25.7 185.1 -18.5 178.5 -57.8 162.6C-97.2 146.6 -131.7 121.3 -149 87.8C-166.2 54.2 -166.2 12.5 -162.2 -31.8C-158.1 -76 -150.1 -122.6 -122.4 -149.7C-94.7 -176.9 -47.4 -184.4 -3.3 -180.5C40.7 -176.5 81.4 -161 118.4 -135.5"
+                      fill="#FFFFFF"
+                    ></path>
+                  </g>
+                </svg>
+              </button>
+              <h3 className="-mt-[30px] text-center text-[2rem]">Dataset</h3>
+            </div>
+            <div>
+              <button className="hover:cursor-pointer">
+                <svg
+                  id="visual"
+                  viewBox="0 0 450 450"
+                  width="450"
+                  height="450"
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="450"
+                    height="450"
+                    fill="#204116"
+                  ></rect>
+                  <g transform="translate(226.66265055739558 215.8023946868828)">
+                    <path
+                      d="M119.1 -144.7C147.3 -118.2 158.3 -74 164.2 -30.5C170.1 13.1 170.8 56 153.6 91.4C136.4 126.8 101.4 154.5 60 173.3C18.6 192 -29.2 201.7 -70.7 188.9C-112.2 176.1 -147.5 140.8 -162.3 100.7C-177.2 60.6 -171.7 15.6 -159.6 -23.8C-147.4 -63.2 -128.7 -97.1 -101 -123.7C-73.3 -150.3 -36.6 -169.7 4.4 -174.9C45.4 -180.1 90.8 -171.3 119.1 -144.7"
+                      fill="#FFFFFF"
+                    ></path>
+                  </g>
+                </svg>
+              </button>
+              <h3 className="-mt-[30px] text-center text-[2rem]">Algorithm</h3>
+            </div>
+            <div>
+              <button className="hover:cursor-pointer">
+                <svg
+                  id="visual"
+                  viewBox="0 0 450 450"
+                  width="450"
+                  height="450"
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="450"
+                    height="450"
+                    fill="#204116"
+                  ></rect>
+                  <g transform="translate(219.1576523566743 210.8573209521628)">
+                    <path
+                      d="M128.9 -145.3C164.9 -123.5 190.3 -80.8 199.2 -34.5C208.2 11.8 200.6 61.7 177.1 102.1C153.6 142.6 114.1 173.7 70.5 186.5C26.9 199.2 -20.7 193.6 -65.7 178.2C-110.6 162.8 -152.7 137.6 -174 100.6C-195.2 63.5 -195.6 14.5 -181.7 -26.4C-167.7 -67.2 -139.5 -99.9 -106.8 -122.2C-74.1 -144.6 -37.1 -156.7 4.7 -162.3C46.5 -167.9 93 -167.1 128.9 -145.3"
+                      fill="#FFFFFF"
+                    ></path>
+                  </g>
+                </svg>
+              </button>
+              <h3 className="-mt-[30px] text-center text-[2rem]">Usage</h3>
+            </div>
+          </div>
         </section>
         <svg
           className="h-[100px] w-full rotate-180"
