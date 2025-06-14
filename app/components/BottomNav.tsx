5347f33964e5ac4e195b91d2031e1aafe45286ae
Lazy Sock <paul.k@unity-mail.de>
2025-06-14 12:42:06 +0200

    add links for BottomNav.tsx

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const tabs = [
   {
@@ -13,7 +14,7 @@ const tabs = [
         <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
       </svg>
     ),
-    label: "Home",
+    label: "app",
   },
   {
     icon: (
@@ -59,7 +60,7 @@ const tabs = [
 export const BottomNav = () => {
   const [active, setActive] = useState(0);
   const [bubbleLeft, setBubbleLeft] = useState(0);
-  const buttonRefs = useRef<(HTMLButtonElement | null)[]>([]);
+  const buttonRefs = useRef<(HTMLAnchorElement | null)[]>([]);
 
   useEffect(() => {
     const el = buttonRefs.current[active];
@@ -104,7 +105,8 @@ export const BottomNav = () => {
           }}
         >
           {tabs.map((tab, index) => (
-            <button
+            <Link
+              to={`/${tab.label.toLowerCase()}`}
               key={index}
               ref={(el) => {
                 buttonRefs.current[index] = el;
@@ -125,7 +127,7 @@ export const BottomNav = () => {
                   active === index ? "bg-white" : "bg-transparent"
                 }`}
               />
-            </button>
+            </Link>
           ))}
         </div>
 
