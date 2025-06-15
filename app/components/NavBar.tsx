cc8f56f4feb77d357774a78c5d324a5532334d35
Ben Seyer <benseyer@web.de>
2025-06-15 14:02:27 +0200

    add hidden Dropdown

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -160,11 +160,11 @@ export default function NavBar() {
         </svg>
         <p className="dark:text-dark-white h-fit text-black">Capybara</p>
       </Link>
-      <div className="mr-10">
+      <div className="mr-10 block md:hidden">
         <Dropdown
           label="Menü öffnen"
           options={[
-            <Link to="/hardware" className="flex gap-2">
+            <Link to="/hardware" className="flex gap-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
@@ -175,7 +175,7 @@ export default function NavBar() {
               </svg>
               <p className="dark:text-dark-primary text-black">Hardware</p>
             </Link>,
-            <Link to="/ai" className="flex gap-2">
+            <Link to="/ai" className="flex gap-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
@@ -186,7 +186,7 @@ export default function NavBar() {
               </svg>
               <p className="dark:text-dark-primary text-black">AI</p>
             </Link>,
-            <Link to="/code" className="flex gap-2">
+            <Link to="/code" className="flex gap-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 640 512"
@@ -197,7 +197,7 @@ export default function NavBar() {
               </svg>
               <p className="dark:text-dark-primary text-black">Code</p>
             </Link>,
-            <button className="flex gap-2">
+            <button className="flex gap-4">
               <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
                 <title>ionicons-v5-q</title>
                 <line
@@ -322,6 +322,21 @@ export default function NavBar() {
           ]}
         />
       </div>
+      <div className="mr-12 hidden lg:flex max-w-2xl gap-12 flex-row text-2xl">
+        <Link to="/hardware" className="flex gap-2 items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-black dark:fill-dark-white" width={35}><path d="M176 24c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40c-35.3 0-64 28.7-64 64l-40 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l40 0 0 56-40 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l40 0 0 56-40 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l40 0c0 35.3 28.7 64 64 64l0 40c0 13.3 10.7 24 24 24s24-10.7 24-24l0-40 56 0 0 40c0 13.3 10.7 24 24 24s24-10.7 24-24l0-40 56 0 0 40c0 13.3 10.7 24 24 24s24-10.7 24-24l0-40c35.3 0 64-28.7 64-64l40 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-40 0 0-56 40 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-40 0 0-56 40 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-40 0c0-35.3-28.7-64-64-64l0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40-56 0 0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40-56 0 0-40zM160 128l192 0c17.7 0 32 14.3 32 32l0 192c0 17.7-14.3 32-32 32l-192 0c-17.7 0-32-14.3-32-32l0-192c0-17.7 14.3-32 32-32zm192 32l-192 0 0 192 192 0 0-192z"/></svg>
+          <p className="text-black dark:text-dark-white">Hardware</p>
+        </Link>
+        <Link to="/ai" className="flex gap-2 items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-black dark:fill-dark-white" width={35}><path d="M184 0c30.9 0 56 25.1 56 56l0 400c0 30.9-25.1 56-56 56c-28.9 0-52.7-21.9-55.7-50.1c-5.2 1.4-10.7 2.1-16.3 2.1c-35.3 0-64-28.7-64-64c0-7.4 1.3-14.6 3.6-21.2C21.4 367.4 0 338.2 0 304c0-31.9 18.7-59.5 45.8-72.3C37.1 220.8 32 207 32 192c0-30.7 21.6-56.3 50.4-62.6C80.8 123.9 80 118 80 112c0-29.9 20.6-55.1 48.3-62.1C131.3 21.9 155.1 0 184 0zM328 0c28.9 0 52.6 21.9 55.7 49.9c27.8 7 48.3 32.1 48.3 62.1c0 6-.8 11.9-2.4 17.4c28.8 6.2 50.4 31.9 50.4 62.6c0 15-5.1 28.8-13.8 39.7C493.3 244.5 512 272.1 512 304c0 34.2-21.4 63.4-51.6 74.8c2.3 6.6 3.6 13.8 3.6 21.2c0 35.3-28.7 64-64 64c-5.6 0-11.1-.7-16.3-2.1c-3 28.2-26.8 50.1-55.7 50.1c-30.9 0-56-25.1-56-56l0-400c0-30.9 25.1-56 56-56z"/></svg>
+          <p className="text-black dark:text-dark-white">AI</p>
+        </Link>
+        <Link to="/code" className="flex gap-2 items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="fill-black dark:fill-dark-white" width={35}><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>
+          <p className="text-black dark:text-dark-white">Code</p>
+        </Link>
+      </div>
+
     </nav>
   );
 }
