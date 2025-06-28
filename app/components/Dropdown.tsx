1801c33de57d352f0aef35b4ce41dbb09a71e50a
Lazy Sock <paul.k@unity-mail.de>
2025-06-28 22:27:32 +0200

    improve Dropdown

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -30,16 +30,24 @@ const Dropdown: React.FC<DropdownProps> = ({ label, options }) => {
     <div className="relative inline-block text-left" ref={dropdownRef}>
       <button
         type="button"
-        className="inline-flex w-full justify-center px-4 py-2 text-sm font-medium text-gray-700 shadow-sm dark:hover:bg-darker-black hover:bg-green-50 focus:outline-none rounded-2xl"
+        className={
+          "dark:hover:bg-darker-black inline-flex w-full cursor-pointer justify-center rounded-2xl px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-green-50 focus:outline-none " +
+          (isOpen ? " dark:bg-darker-black bg-green-50" : "")
+        }
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-6 dark:fill-white fill-black"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
-
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          className="w-6 fill-black dark:fill-white"
+        >
+          <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+        </svg>
       </button>
 
       {isOpen && (
-        <div className="ring-opacity-5 absolute mt-2 right-0 rounded-md bg-white shadow-lg ring-1 ring-black focus:outline-none z-50">
-          <div className="py-1 space-y-1 px-2">
+        <div className="ring-opacity-5 absolute right-0 z-50 mt-2 rounded-md bg-white shadow-lg ring-1 ring-black focus:outline-none">
+          <div className="space-y-1 px-2 py-1">
             {options.map((option, index) => (
               <div key={index} className="w-full">
                 {option}
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -50,52 +50,160 @@ export default function NavBar() {
         <Dropdown
           label="Menü öffnen"
           options={[
-            <Link to="/hardware" className="flex gap-4">
+            <Link to="/hardware" className="flex gap-[8px]">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
-                className="dark:fill-dark-primary fill-black"
-                width={25}
+                className="dark:fill-dark-primary w-[22px] fill-black"
               >
                 <path d="M176 24c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40c-35.3 0-64 28.7-64 64l-40 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l40 0 0 56-40 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l40 0 0 56-40 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l40 0c0 35.3 28.7 64 64 64l0 40c0 13.3 10.7 24 24 24s24-10.7 24-24l0-40 56 0 0 40c0 13.3 10.7 24 24 24s24-10.7 24-24l0-40 56 0 0 40c0 13.3 10.7 24 24 24s24-10.7 24-24l0-40c35.3 0 64-28.7 64-64l40 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-40 0 0-56 40 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-40 0 0-56 40 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-40 0c0-35.3-28.7-64-64-64l0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40-56 0 0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40-56 0 0-40zM160 128l192 0c17.7 0 32 14.3 32 32l0 192c0 17.7-14.3 32-32 32l-192 0c-17.7 0-32-14.3-32-32l0-192c0-17.7 14.3-32 32-32zm192 32l-192 0 0 192 192 0 0-192z" />
               </svg>
               <p className="dark:text-dark-primary text-black">Hardware</p>
             </Link>,
-            <Link to="/ai" className="flex gap-4">
+            <Link to="/ai" className="flex gap-[8px]">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
-                className="dark:fill-dark-primary fill-black"
-                width={25}
+                className="dark:fill-dark-primary w-[22px] fill-black"
               >
                 <path d="M184 0c30.9 0 56 25.1 56 56l0 400c0 30.9-25.1 56-56 56c-28.9 0-52.7-21.9-55.7-50.1c-5.2 1.4-10.7 2.1-16.3 2.1c-35.3 0-64-28.7-64-64c0-7.4 1.3-14.6 3.6-21.2C21.4 367.4 0 338.2 0 304c0-31.9 18.7-59.5 45.8-72.3C37.1 220.8 32 207 32 192c0-30.7 21.6-56.3 50.4-62.6C80.8 123.9 80 118 80 112c0-29.9 20.6-55.1 48.3-62.1C131.3 21.9 155.1 0 184 0zM328 0c28.9 0 52.6 21.9 55.7 49.9c27.8 7 48.3 32.1 48.3 62.1c0 6-.8 11.9-2.4 17.4c28.8 6.2 50.4 31.9 50.4 62.6c0 15-5.1 28.8-13.8 39.7C493.3 244.5 512 272.1 512 304c0 34.2-21.4 63.4-51.6 74.8c2.3 6.6 3.6 13.8 3.6 21.2c0 35.3-28.7 64-64 64c-5.6 0-11.1-.7-16.3-2.1c-3 28.2-26.8 50.1-55.7 50.1c-30.9 0-56-25.1-56-56l0-400c0-30.9 25.1-56 56-56z" />
               </svg>
               <p className="dark:text-dark-primary text-black">AI</p>
             </Link>,
-            <Link to="/code" className="flex gap-4">
+            <Link to="/code" className="flex gap-[8px]">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 640 512"
-                className="dark:fill-dark-primary fill-black"
-                width={25}
+                className="dark:fill-dark-primary w-[22px] fill-black"
               >
                 <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" />
               </svg>
               <p className="dark:text-dark-primary text-black">Code</p>
             </Link>,
             <button className="flex gap-4">
-                <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" width={25}>
-                  <title>ionicons-v5-q</title>
-                  <line x1="256" y1="48" x2="256" y2="96" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <line x1="256" y1="416" x2="256" y2="464" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <line x1="403.08" y1="108.92" x2="369.14" y2="142.86" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <line x1="142.86" y1="369.14" x2="108.92" y2="403.08" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <line x1="464" y1="256" x2="416" y2="256" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <line x1="96" y1="256" x2="48" y2="256" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <line x1="403.08" y1="403.08" x2="369.14" y2="369.14" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <line x1="142.86" y1="142.86" x2="108.92" y2="108.92" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                  <circle cx="256" cy="256" r="80" style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeMiterlimit: 10, strokeWidth: "55px" }} />
-                </svg>
+              <svg
+                viewBox="0 0 512 512"
+                xmlns="http://www.w3.org/2000/svg"
+                width={25}
+              >
+                <title>ionicons-v5-q</title>
+                <line
+                  x1="256"
+                  y1="48"
+                  x2="256"
+                  y2="96"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <line
+                  x1="256"
+                  y1="416"
+                  x2="256"
+                  y2="464"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <line
+                  x1="403.08"
+                  y1="108.92"
+                  x2="369.14"
+                  y2="142.86"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <line
+                  x1="142.86"
+                  y1="369.14"
+                  x2="108.92"
+                  y2="403.08"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <line
+                  x1="464"
+                  y1="256"
+                  x2="416"
+                  y2="256"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <line
+                  x1="96"
+                  y1="256"
+                  x2="48"
+                  y2="256"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <line
+                  x1="403.08"
+                  y1="403.08"
+                  x2="369.14"
+                  y2="369.14"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <line
+                  x1="142.86"
+                  y1="142.86"
+                  x2="108.92"
+                  y2="108.92"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+                <circle
+                  cx="256"
+                  cy="256"
+                  r="80"
+                  style={{
+                    fill: "none",
+                    stroke: "#000000",
+                    strokeLinecap: "round",
+                    strokeMiterlimit: 10,
+                    strokeWidth: "55px",
+                  }}
+                />
+              </svg>
               <p className="dark:text-dark-primary text-black">Dark</p>
             </button>,
           ]}
@@ -135,7 +243,7 @@ export default function NavBar() {
           </svg>
           <p className="dark:text-dark-white text-black">Code</p>
         </Link>
-          {/* <LanguageSwitch /> */}
+        {/* <LanguageSwitch /> */}
       </div>
     </nav>
   );
