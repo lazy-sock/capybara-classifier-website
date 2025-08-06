d462efc0bacc69a08100df8d0811f774cec68f5f
Ben Seyer <benseyer@web.de>
2025-08-06 20:41:09 +0200

    add design for explanation of Code

--- app/routes/code.tsx
+++ app/routes/code.tsx
@@ -74,9 +74,9 @@ const Training = `
 
 export default function network() {
     return (
-        <div>
+        <main>
             <NavBar />
-            <main className="mx-auto w-full max-w-[1000px] pt-30">
+            <div className="mx-auto w-full max-w-[1000px] pt-30">
               <div className="flex flex-col space-y-12 p-5 text-black dark:text-dark-white">
                 <h1 className="text-center text-[3rem] font-bold">Das Netzwerk</h1>
                 <div className="flex flex-col space-y-5">                
@@ -128,37 +128,67 @@ export default function network() {
                   <CodeBlock code={Network[5]}/>
                 </div>
               </div>
-              <div className="flex flex-col space-y-12 p-5 text-black dark:text-dark-white">
-                <h1 className="text-center text-[3rem] font-bold">Die Transformationen</h1>
-                <p>
-                  Dieser Code definiert eine Reihe von Bild-Transformationsschritten zur Datenaugmentation im Training eines neuronalen Netzes, um die Robustheit des Modells zu verbessern. Die Transformationen besteht im Einzelnen aus:
-                  <br/>
-                  1.  Resize: Das Bild wird zunächst vergrößert, um später ein zentriertes Zuschneiden zu ermöglichen.
-                  <CodeBlock code={TrainingTransforms[0]}/>
-                  2.  RandomCrop: Ein zufälliger Bildausschnitt der Zielgröße wird gewählt, um unterschiedliche Bildbereiche zu betrachten.
-                  <CodeBlock code={TrainingTransforms[1]}/>
-                  3.  RandomHorizontalFlip: Mit einer Wahrscheinlichkeit von 50 % wird das Bild horizontal gespiegelt, um Symmetrien auszunutzen.
-                  <CodeBlock code={TrainingTransforms[2]}/>
-                  4.  RandomRotation: Das Bild wird zufällig um bis zu 15° gedreht, um Rotationsinvarianz zu fördern.
-                  <CodeBlock code={TrainingTransforms[3]}/>
-                  5.  ColorJitter: Helligkeit, Kontrast, Sättigung und Farbton werden leicht variiert, um unterschiedliche Lichtverhältnisse zu simulieren.
-                  <CodeBlock code={TrainingTransforms[4]}/>
-                  6.  RandomAffine: Leichte Translationen und Skalierungen simulieren weitere Variationen in Position und Größe der Objekte.
-                  <CodeBlock code={TrainingTransforms[5]}/>
-                  7.  ToTensor: Das Bild wird in ein Tensor-Format umgewandelt, das von PyTorch verarbeitet werden kann.
-                  <CodeBlock code={TrainingTransforms[6]}/>
-                  8.  Normalize: Die Pixelwerte werden normalisiert, um den Mittelwert und die Standardabweichung der ImageNet-Daten nachzubilden und das Training zu stabilisieren.
-                  <CodeBlock code={TrainingTransforms[7]}/>
-                </p>
+            </div>
+            <svg
+              className="h-[100px] w-full"
+              viewBox="0 460 960 80"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M0 475L26.7 480.8C53.3 486.7 106.7 498.3 160 501.5C213.3 504.7 266.7 499.3 320 489.7C373.3 480 426.7 466 480 464C533.3 462 586.7 472 640 483.2C693.3 494.3 746.7 506.7 800 503.2C853.3 499.7 906.7 480.3 933.3 470.7L960 461L960 541L933.3 541C906.7 541 853.3 541 800 541C746.7 541 693.3 541 640 541C586.7 541 533.3 541 480 541C426.7 541 373.3 541 320 541C266.7 541 213.3 541 160 541C106.7 541 53.3 541 26.7 541L0 541Z"
+                fill="#204116"
+              />
+            </svg>
+            <div className="bg-primary">
+              <div className="mx-auto w-full max-w-[1000px] pt-10 ">
+                <div className="flex flex-col space-y-12 p-5 text-white dark:text-dark-white">
+                  <h1 className="text-center text-[3rem] font-bold">Die Transformationen</h1>
+                  <p>
+                    Dieser Code definiert eine Reihe von Bild-Transformationsschritten zur Datenaugmentation im Training eines neuronalen Netzes, um die Robustheit des Modells zu verbessern. Die Transformationen besteht im Einzelnen aus:
+                    <br/>
+                    1.  Resize: Das Bild wird zunächst vergrößert, um später ein zentriertes Zuschneiden zu ermöglichen.
+                    <CodeBlock code={TrainingTransforms[0]}/>
+                    2.  RandomCrop: Ein zufälliger Bildausschnitt der Zielgröße wird gewählt, um unterschiedliche Bildbereiche zu betrachten.
+                    <CodeBlock code={TrainingTransforms[1]}/>
+                    3.  RandomHorizontalFlip: Mit einer Wahrscheinlichkeit von 50 % wird das Bild horizontal gespiegelt, um Symmetrien auszunutzen.
+                    <CodeBlock code={TrainingTransforms[2]}/>
+                    4.  RandomRotation: Das Bild wird zufällig um bis zu 15° gedreht, um Rotationsinvarianz zu fördern.
+                    <CodeBlock code={TrainingTransforms[3]}/>
+                    5.  ColorJitter: Helligkeit, Kontrast, Sättigung und Farbton werden leicht variiert, um unterschiedliche Lichtverhältnisse zu simulieren.
+                    <CodeBlock code={TrainingTransforms[4]}/>
+                    6.  RandomAffine: Leichte Translationen und Skalierungen simulieren weitere Variationen in Position und Größe der Objekte.
+                    <CodeBlock code={TrainingTransforms[5]}/>
+                    7.  ToTensor: Das Bild wird in ein Tensor-Format umgewandelt, das von PyTorch verarbeitet werden kann.
+                    <CodeBlock code={TrainingTransforms[6]}/>
+                    8.  Normalize: Die Pixelwerte werden normalisiert, um den Mittelwert und die Standardabweichung der ImageNet-Daten nachzubilden und das Training zu stabilisieren.
+                    <CodeBlock code={TrainingTransforms[7]}/>
+                  </p>
+                </div>
               </div>
-              <div className="flex flex-col space-y-12 p-5 text-black dark:text-dark-white">
-                <h1 className="text-center text-[3rem] font-bold">Das Training</h1>
+            </div>
+            <svg
+              className="h-[100px] w-full rotate-180"
+              viewBox="0 460 960 60"
+              width="100%"
+              height="100"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M0 503L26.7 499.5C53.3 496 106.7 489 160 480.8C213.3 472.7 266.7 463.3 320 469C373.3 474.7 426.7 495.3 480 495.8C533.3 496.3 586.7 476.7 640 472.8C693.3 469 746.7 481 800 487C853.3 493 906.7 493 933.3 493L960 493L960 540L933.3 540C906.7 540 853.3 540 800 540C746.7 540 693.3 540 640 540C586.7 540 533.3 540 480 540C426.7 540 373.3 540 320 540C266.7 540 213.3 540 160 540C106.7 540 53.3 540 26.7 540L0 540Z"
+                fill="#204116"
+              />
+            </svg>
+            <div className="mx-auto w-full max-w-[1000px] pt-10">
+              <div className="flex flex-col space-y-10 p-5 text-black dark:text-dark-white">
+                <h1 className="text-center text-[4rem] font-bold">Das Training</h1>
                 <p>
                   Der gezeigte Code beschreibt eine Trainingsschleife für ein neuronales Netzwerk mit PyTorch. Dabei werden in jeder Iteration des Trainingsdatenladers ein Batch von Eingabedaten und den dazugehörigen Zielwerten auf das entsprechende Gerät (CPU oder GPU) verschoben. Anschließend werden die zuvor berechneten Gradienten im Optimierer zurückgesetzt, um eine korrekte Aktualisierung der Gewichte zu gewährleisten. Das Modell verarbeitet die Eingabedaten und liefert Vorhersagen, anhand derer der Verlust mithilfe einer Verlustfunktion berechnet wird. Danach wird der Verlust rückwärts propagiert, um die Gradienten zu ermitteln, und der Optimierer passt die Modellparameter entsprechend an. Zusätzlich wird der Verlustwert akkumuliert, um den Trainingsfortschritt zu verfolgen. Die Vorhersagen werden ausgewertet, indem für jede Eingabe die Klasse mit der höchsten Wahrscheinlichkeit bestimmt und mit den tatsächlichen Zielwerten verglichen wird, um die Anzahl der korrekten Vorhersagen zu zählen. Schließlich wird alle 50 Batches der aktuelle Verlustwert ausgegeben, um eine laufende Überwachung des Trainings zu ermöglichen.                </p>
                 <CodeBlock code={Training}/>
               </div>
-            </main>
+            </div>
             <Footer />
-        </div>
+        </main>
     );
 }
